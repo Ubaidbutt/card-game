4a68ca5215bc9eff4672b3fbe975c3bd48bb4667
ubaidbutt <ubaidbutt200@gmail.com>
2023-03-18 10:39:16 +0100

    Add a library function for shuffling arrays

---
+++ src/lib/shuffle.ts
@@ -0,0 +1,8 @@
+export function shuffle(numbers: number[]): number[] {
+    const shuffled: number[] = [...numbers];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+}

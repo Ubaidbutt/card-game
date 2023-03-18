716adc7898e4f1ce8d3376077e3fc49debe2b549
ubaidbutt <ubaidbutt200@gmail.com>
2023-03-18 11:22:49 +0100

    Update index.js file to handle errors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,21 @@ import Game from "./Game";
 import Player from "./Player";
 import { shuffle } from './lib/shuffle';
 
-const deck1 = new Deck(52);
-deck1.shuffleCards(shuffle);
-const cards = deck1.splitCards(2);
+function main() {
+    try {
+        const deck1 = new Deck(52);
+        deck1.shuffleCards(shuffle);
+        const cards = deck1.splitCards(2);
+      
+        const p1 = new Player(cards[0], 1);
+        const p2 = new Player(cards[1], 2);
+      
+        const game = new Game([p1, p2]);
+        game.play();
+    } catch (error) {
+        console.error("An error occurred:", error);
+        process.exit(1);
+    }
+}
 
-const p1 = new Player(cards[0], 1);
-const p2 = new Player(cards[1], 2);
-
-const game = new Game([p1, p2]);
-game.play();
+main();

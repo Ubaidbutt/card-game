71e6b13a298a53deef69127dc3eae2b1171d9ee6
ubaidbutt <ubaidbutt200@gmail.com>
2023-03-18 10:39:54 +0100

    Added index.js file for playing the game

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world');
+import Deck from "./Deck";
+import Game from "./Game";
+import Player from "./Player";
+import { shuffle } from './lib/shuffle';
+
+const deck1 = new Deck(52);
+deck1.shuffleCards(shuffle);
+const cards = deck1.splitCards(2);
+
+const p1 = new Player(cards[0], 1);
+const p2 = new Player(cards[1], 2);
+
+const game = new Game([p1, p2]);
+game.play();
